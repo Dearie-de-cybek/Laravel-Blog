3115536d6b68028ab4d95fa93f777696527ab321
Eburu Dearie <eburujacintha@gmail.com>
2023-02-06 11:44:02 +0200

    updated route file

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
  
 Route::get('/', function () {
-    return view('posts', ['posts' => Post::latest()->get(),
-                         'categories' => Category::all()
+    return view('posts', ['posts' => Post::latest()->get(),'categories' => Category::all()
     ]);
 });
 
