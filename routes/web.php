d9b32951ac217cce5a7ef5d7a720fa6dde8bd5f8
Eburu Dearie <eburujacintha@gmail.com>
2023-02-06 12:25:05 +0200

    Fixed the category display issue

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,13 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post:slug}', function (Post $post){
-    return view('post', ['post' => $post]);
+    return view('post', ['post' => $post, 'categories' => Category::all()]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', ['posts' => $category->posts]);
+    return view('posts', ['posts' => $category->posts, 'currentCategory' =>$category, 'categories' => Category::all()]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', ['posts' => $author->posts]);
+    return view('posts', ['posts' => $author->posts, 'categories' => Category::all()]);
 });
