5618b50107ebaf853aa2e2b5b5d63fb8fe02cc3e
Eburu Dearie <eburujacintha@gmail.com>
2023-02-23 11:55:26 +0200

    Search function working

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -7,7 +7,7 @@
     <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
                                                        alt="Head of Lary the mascot"></h2>
 
-    <p class="text-sm mt-14">
+    <p class="text-sm mt-4">
         Another year. Another update. We're refreshing the popular Laravel series with new content.
         I'm going to keep you guys up to speed with what's going on!
     </p>
@@ -19,22 +19,16 @@
             <x-slot name="trigger">
                 <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 flex text-left lg:inline-flex">
                     {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-            
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                 height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-            </svg>
+
+                    <x-icon  name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
+
+                  
                 </button>
             </x-slot>
 
             <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
             @foreach ($categories as $category)
-            <x-dropdown-item href="/categories/{{ $category->slug }}" :active="request()->is("categories/ $category->slug)">{{ ucwords($category->name) }}</x-dropdown-item>
+            <x-dropdown-item href="/categories/{{ $category->slug }}" :active="request()->is('$category->slug')" >{{ ucwords($category->name) }}</x-dropdown-item>
             {{-- <a {{  ? 'bg-blue-500 text-white' : ''}}>{{ ucwords($category->name) }}</a> --}}
            
             @endforeach
@@ -42,7 +36,7 @@
         </div>
 
         <!-- Other Filters -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+        {{-- <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Other Filters
                 </option>
@@ -61,13 +55,14 @@
                           d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
                 </g>
             </svg>
-        </div>
+        </div> --}}
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
                 <input type="text" name="search" placeholder="Find something"
-                       class="bg-transparent placeholder-black font-semibold text-sm">
+                       class="bg-transparent placeholder-black font-semibold text-sm"
+                       value="{{ request('search') }}">
             </form>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
  
-Route::get('/', function () {
-    return view('posts', ['posts' => Post::latest()->get(),'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post){
-    return view('post', ['post' => $post, 'categories' => Category::all()]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', ['posts' => $category->posts, 'currentCategory' =>$category, 'categories' => Category::all()]);
