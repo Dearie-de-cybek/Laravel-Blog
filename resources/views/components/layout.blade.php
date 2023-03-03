5c500876eac35a98e3a87c1bc650cd79e6eaf11f
Eburu Dearie <eburujacintha@gmail.com>
2023-03-03 16:06:26 +0200

    Changed title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 
-<title>Techies Guide</title>
+<title>Dearie's Blog</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
