fd45b18eacdc3d729585f6158572d310f8fbad11
Eburu Dearie <eburujacintha@gmail.com>
2023-03-06 14:02:00 +0200

    Created RegisterController

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts.index', ['posts' => $author->posts]);
 });  
+Route::get('register', [RegisterController::class, 'create']);
