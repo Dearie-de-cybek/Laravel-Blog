6f7093f1a5335a7cd9932824e03989327d0db646
Eburu Dearie <eburujacintha@gmail.com>
2023-03-25 00:46:15 +0200

    Fininshed register page

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -2,18 +2,29 @@
    
 @section('content')
             <h1 class="text-center font-bold text-xl">Register!</h1>
-            <form method="POST" action="/register">
-            <div class="mb-6">
-                <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">Username</label>
+            <main class="max-w-lg mx-auto mt-10 bg-gray-100 p-6 border border-gray-200 rounded-xl">
 
-                <input type="text" name="username" id="username" required class="border border-gray-400 p-2 w-full">
-                @error('username')
-                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-            </form>
+                <form method="POST" action="/register" class="mt-10">
+                    <div class="mb-6">
+                        <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
+                        <input type="text" name="name" id="name" required class="border border-gray-400 p-2 w-full">
+                        
+                        <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">Username</label>
+                        <input type="text" name="username" id="username" required class="border border-gray-400 p-2 w-full">
 
-            {{-- Why wont you work --}}
+                        <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
+                        <input type="password" name="password" id="password" required class="border border-gray-400 p-2 w-full"> 
+                    </div>
+                    <div class="mb-6">
+                        <button type="submit"
+                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </main>
+
+           
     
 @endsection
   
