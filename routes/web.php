61c2b4aa54f0bbd39ac08b98384f1a51bd8142d6
Eburu Dearie <eburujacintha@gmail.com>
2023-03-31 23:31:09 +0200

    Layout

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts.index', ['posts' => $author->posts]);
 });  
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
