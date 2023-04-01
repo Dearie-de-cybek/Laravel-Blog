b051eef5c23547dcbb1454b9de472872d2e77f7a
Eburu Dearie <eburujacintha@gmail.com>
2023-04-02 00:36:44 +0200

    updated comments

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,13 @@
+<article class="flex bg-gray-100 border border-gray-300 p-7 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="User avatar" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Jon Doe</h3>
+            <p class="text-xs">Posted <time>8 months ago </time> </p>
+        </header>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quisquam deserunt quia, eligendi libero optio nam assumenda at obcaecati vitae sit pariatur voluptates, ut neque praesentium. Voluptatum voluptatibus error ea.</p>
+    </div>
+</article>
