3a6a9f6fd221293af6a0b661f652637344259fec
Eburu Dearie <eburujacintha@gmail.com>
2023-04-07 00:02:27 +0200

    ForeignID consistency maintained

--- database/migrations/2023_04_01_084515_create_comments_table.php
+++ database/migrations/2023_04_01_084515_create_comments_table.php
@@ -15,14 +15,19 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
+
+            
+
+            //Old Method ne wmethod on line18
+            // $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
         });
     }
 
-    /**
+    /** 
      * Reverse the migrations.
      *
      * @return void
