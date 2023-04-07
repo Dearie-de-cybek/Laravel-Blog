be755aee676fcab42343ebaf1e73d09219e4f247
Eburu Dearie <eburujacintha@gmail.com>
2023-04-07 14:42:42 +0200

    Comment form added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,6 +51,25 @@
             </div>
         </div>
         <section class="col-span-8 col-start-5 mt-10 space-y-6">
+            <x-panel>
+                
+                <form method="POST" action="">
+                    @csrf
+                    <header class="flex items-center">
+                        <img src="https://i.pravatar.cc/100?u={{ auth()->id()}}" alt="User avatar" width="40" height="40" class="rounded-full">
+                        <h2 class="ml-4">Want to Participate?</h2>
+                    </header>
+                    <div class="mt-5">
+                        <textarea name="body"  rows="5" class="w-full text-sm focus:outline-none focus:ring" placeholder="Think, of something to say"></textarea>
+                    </div>
+                    
+                    <div class="flex justify-end mt-5 pt-5 border-t border-gray ">
+                        <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                    </div>
+                    
+                </form>
+            </x-panel>
+
             @foreach ($post->comments as $comment)
             <x-post-comment :comment="$comment"/>
             @endforeach
